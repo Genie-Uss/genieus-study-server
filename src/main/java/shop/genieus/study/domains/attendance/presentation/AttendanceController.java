0c9cb7cfec678f502cda542665d258f3d2a30638
Soobin Kim <xpsxm225@naver.com>
2025-05-10 16:21:51 +0900

    feat: `/attendances/check-in` rest api 정의
    - CheckInRequest: request body, 출석 일시 필수
    - CheckInResponse: response body
    - CheckInInfo: 응용 계층 dto, 유저 아이디와 출석 일시 요구
    
    Resolved #2

---
+++ src/main/java/shop/genieus/study/domains/attendance/presentation/AttendanceController.java
@@ -0,0 +1,32 @@
+package shop.genieus.study.domains.attendance.presentation;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shop.genieus.study.domains.attendance.application.AttendanceService;
+import shop.genieus.study.domains.attendance.application.dto.info.CheckInInfo;
+import shop.genieus.study.domains.attendance.presentation.dto.request.CheckInRequest;
+import shop.genieus.study.domains.attendance.presentation.dto.response.CheckInResponse;
+import shop.genieus.study.domains.auth.presentation.annotation.AuthPrincipal;
+import shop.genieus.study.domains.auth.presentation.dto.CustomPrincipal;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/attendances")
+public class AttendanceController {
+  private final AttendanceService attendanceService;
+
+  @PostMapping("/check-in")
+  public ResponseEntity<CheckInResponse> checkIn(
+      @AuthPrincipal CustomPrincipal principal, @RequestBody @Valid CheckInRequest request) {
+    CheckInResponse response =
+        CheckInResponse.from(
+            attendanceService.checkIn(new CheckInInfo(principal.id(), request.checkInDateTime())));
+
+    return ResponseEntity.ok().body(response);
+  }
+}
