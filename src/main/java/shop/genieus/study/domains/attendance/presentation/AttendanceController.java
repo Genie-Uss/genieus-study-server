952fa8328430d8a549e092ae8e8e1a17c2bbba32
Soobin Kim <xpsxm225@naver.com>
2025-05-10 16:39:12 +0900

    feat: `/attendances/check-out` rest api 정의
    - CheckOutRequest: request body, 퇴실 일시 필수
    - CheckOutResponse: response body
    - CheckOutInfo: 응용 계층 dto, 유저 아이디와 퇴실 일시 요구
    
    Resolved #2

--- src/main/java/shop/genieus/study/domains/attendance/presentation/AttendanceController.java
+++ src/main/java/shop/genieus/study/domains/attendance/presentation/AttendanceController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.genieus.study.domains.attendance.application.AttendanceService;
 import shop.genieus.study.domains.attendance.application.dto.info.CheckInInfo;
+import shop.genieus.study.domains.attendance.application.dto.info.CheckOutInfo;
 import shop.genieus.study.domains.attendance.presentation.dto.request.CheckInRequest;
+import shop.genieus.study.domains.attendance.presentation.dto.request.CheckOutRequest;
 import shop.genieus.study.domains.attendance.presentation.dto.response.CheckInResponse;
+import shop.genieus.study.domains.attendance.presentation.dto.response.CheckOutResponse;
 import shop.genieus.study.domains.auth.presentation.annotation.AuthPrincipal;
 import shop.genieus.study.domains.auth.presentation.dto.CustomPrincipal;
 
@@ -26,6 +29,17 @@ public class AttendanceController {
     CheckInResponse response =
         CheckInResponse.from(
             attendanceService.checkIn(new CheckInInfo(principal.id(), request.checkInDateTime())));
+
+    return ResponseEntity.ok().body(response);
+  }
+
+  @PostMapping("/check-out")
+  public ResponseEntity<CheckOutResponse> checkOut(
+      @AuthPrincipal CustomPrincipal principal, @RequestBody @Valid CheckOutRequest request) {
+    CheckOutResponse response =
+        CheckOutResponse.from(
+            attendanceService.checkOut(
+                new CheckOutInfo(principal.id(), request.checkOutDateTime())));
 
     return ResponseEntity.ok().body(response);
   }
