bba49fd0358bc33ef8e81ae34a6220a584556eb3
seongmin <seongmin1117@gmail.com>
2025-05-12 20:31:30 +0900

    feat: 한국 시간 기본으로 설정

--- src/main/java/shop/genieus/study/commons/provider/DateTimeProvider.java
+++ src/main/java/shop/genieus/study/commons/provider/DateTimeProvider.java
@@ -1,16 +1,21 @@
 package shop.genieus.study.commons.provider;
 
+import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class DateTimeProvider {
+  private  final Clock clock;
+
   public LocalDate getCurrentDate() {
-    return LocalDate.now();
+    return LocalDate.now(clock);
   }
 
   public LocalDateTime getCurrentDateTime() {
-    return LocalDateTime.now();
+    return LocalDateTime.now(clock);
   }
 }
