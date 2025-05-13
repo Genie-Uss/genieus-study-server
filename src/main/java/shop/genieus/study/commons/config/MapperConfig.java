0321f82c0220cfc352badb5b299fb184733eafe2
seongmin <seongmin1117@gmail.com>
2025-05-13 11:20:37 +0900

    feat: 시간 포맷 설정 추가

--- src/main/java/shop/genieus/study/commons/config/MapperConfig.java
+++ src/main/java/shop/genieus/study/commons/config/MapperConfig.java
@@ -2,7 +2,13 @@ package shop.genieus.study.commons.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +17,15 @@ public class MapperConfig {
   @Bean
   public ObjectMapper passportObjectMapper() {
     ObjectMapper mapper = new ObjectMapper();
-    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    mapper.registerModule(new JavaTimeModule());
+    mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    JavaTimeModule javaTimeModule = new JavaTimeModule();
+    javaTimeModule.addSerializer(LocalDateTime.class,
+        new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+    javaTimeModule.addSerializer(LocalDate.class,
+        new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+    mapper.registerModule(javaTimeModule);
     return mapper;
   }
 }
