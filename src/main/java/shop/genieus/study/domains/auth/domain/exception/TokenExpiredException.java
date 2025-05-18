f6c2c558fd4272cca6b089b0c8c95393f3394a9e
Soobinnni <xpsxm225@naver.com>
2025-05-18 22:46:39 +0900

    update: register -> signup으로 이름 변경
    Resolved: #29

--- src/main/java/shop/genieus/study/domains/auth/domain/exception/TokenExpiredException.java
+++ src/main/java/shop/genieus/study/domains/auth/domain/exception/TokenExpiredException.java
@@ -4,13 +4,11 @@ import shop.genieus.study.commons.exception.Domain;
 import shop.genieus.study.commons.exception.ValidationException;
 
 public class TokenExpiredException extends ValidationException {
-  public static final String EXPIRED_TOKEN = "만료된 JWT 토큰입니다.";
-
   public TokenExpiredException(String message) {
     super(message, Domain.AUTH);
   }
 
   public static TokenExpiredException create() {
-    return new TokenExpiredException(EXPIRED_TOKEN);
+    return new TokenExpiredException("만료된 JWT 토큰입니다.");
   }
 }
--- src/main/java/shop/genieus/study/domains/user/presentation/dto/request/SignupUserRequest.java
+++ src/main/java/shop/genieus/study/domains/user/presentation/dto/request/SignupUserRequest.java
@@ -2,14 +2,14 @@ package shop.genieus.study.domains.user.presentation.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import shop.genieus.study.domains.user.application.dto.info.RegisterUserInfo;
+import shop.genieus.study.domains.user.application.dto.info.SignupUserInfo;
 
-public record RegisterUserRequest(
+public record SignupUserRequest(
     @NotBlank(message = "이메일 주소를 입력해 주세요") @Email(message = "이메일 형식이 아닙니다") String email,
     @NotBlank(message = "비밀번호를 입력해 주세요") String password,
     @NotBlank(message = "비밀번호 확인이 입력되지 않았습니다") String confirmPassword,
     @NotBlank(message = "닉네임이 입력되지 않았습니다.") String nickname) {
-  public RegisterUserInfo to() {
-    return new RegisterUserInfo(this.email, this.password, this.confirmPassword, this.nickname);
+  public SignupUserInfo to() {
+    return new SignupUserInfo(this.email, this.password, this.confirmPassword, this.nickname);
   }
 }
