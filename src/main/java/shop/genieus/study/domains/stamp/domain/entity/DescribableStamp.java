28d46e3335fda0c473c50afd331b9d9451cdc2b9
Soobinnni <xpsxm225@naver.com>
2025-06-02 22:03:34 +0900

    feat: DescribableStamp 인터페이스 도입
    - 모든 Stamp 엔티티의 공통 메서드 정의
    - getCategories, getTitle, getContent, getUrl 등 기본 구현 제공
    - 타입별 특화 getter 메서드 정의 (getAlgorithmType, getCategoryType 등)
    
    Resolved: #41

--- src/main/java/shop/genieus/study/domains/stamp/domain/entity/DescribableStamp.java
+++ src/main/java/shop/genieus/study/domains/stamp/domain/entity/DescribableStamp.java
@@ -1,8 +1,10 @@
 package shop.genieus.study.domains.stamp.domain.entity;
 
+import shop.genieus.study.domains.stamp.domain.vo.*;
+
 public interface DescribableStamp {
   default String[] getCategories() {
-    return null;
+    return new String[0];
   }
 
   default String getTitle() {
@@ -14,6 +16,26 @@ public interface DescribableStamp {
   }
 
   default String getUrl() {
+    return null;
+  }
+
+  default AlgorithmType getAlgorithmType() {
+    return null;
+  }
+
+  default PlatformType getPlatformType() {
+    return null;
+  }
+
+  default CategoryType getCategoryType() {
+    return null;
+  }
+
+  default ActivityType getActivityType() {
+    return null;
+  }
+
+  default CareerType getCareerType() {
     return null;
   }
 }
