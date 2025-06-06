0f9472cd2f77f4eba1778e9d83bec0ff82abd471
Soobinnni <xpsxm225@naver.com>
2025-06-07 01:00:54 +0900

    feat: 공통 이벤트 시스템 구현
    - 기존 이벤트 구조에서 새로운 공통 이벤트 시스템으로 마이그레이션
    - DomainEvent 인터페이스 및 DomainEventType enum 추가
    - DomainPayload record로 이벤트 데이터 표준화
      - Builder 패턴으로 유연한 attribute 관리
      - 타입 안전한 attribute 조회 메서드 제공
    - @EventFilter 어노테이션 및 AOP 기반 이벤트 필터링
      - 선언적 이벤트 구독으로 코드 가독성 향상
      - 불필요한 이벤트 처리 방지로 성능 최적화
    - 도메인 간 결합도 감소 및 확장성 개선
    
    Resolved: #44

---
+++ src/main/java/shop/genieus/study/commons/event/DomainEventType.java
@@ -0,0 +1,13 @@
+package shop.genieus.study.commons.event;
+
+public enum DomainEventType {
+  REMIND_CHECK_IN,
+  REMIND_CHECK_OUT,
+  CHECK_IN,
+  CHECK_OUT,
+
+  STAMP_CREATED,
+  STAMP_DELETED,
+
+  DAILY_STATS_CALCULATED,
+}
